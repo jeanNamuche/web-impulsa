89664c388b15bbfc81fed1fa575ee20a1dc386b9
jeanNamuche <jean09namuche@gmail.com>
2025-04-02 13:28:51 -0500

    Update Cursos.js
    mas codigo

--- src/pages/Cursos.js
+++ src/pages/Cursos.js
@@ -5,7 +5,7 @@ const Cursos = () => {
   return (
     <div>
       <h1>Página de Cursos</h1>
-      <p>Explora los cursos disponibles y mejora tus habilidades.</p>
+      <p>Explora los cursos disponibles y mejora tus habilidades. empiezen el codigo </p>
       <h2>Contáctanos</h2>
       <p>Si tienes alguna consulta sobre los cursos, por favor llena el siguiente formulario:</p>
       <div className='datos'><FormularioContacto />

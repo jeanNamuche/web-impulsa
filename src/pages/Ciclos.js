2a9e00823643ef1b9b6bd2583d92d3096492dd2c
Bryanmn0107 <brayan.08.sonic@gmail.com>
2025-04-08 11:55:48 -0500

    Ciclos.js

--- src/pages/Ciclos.js
+++ src/pages/Ciclos.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import CardGrid from '../components/CardGrid';
 const Ciclos = () => {
   return (
     <div>
       <h1>Página de Ciclos</h1>
       <p>Consulta los ciclos académicos y sus detalles.</p>
+      <CardGrid/>
     </div>
   );
 };

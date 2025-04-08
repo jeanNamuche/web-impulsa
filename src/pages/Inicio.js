7361113f4610eb7abe1fbb18902b52eb0bc5493f
Bryanmn0107 <brayan.08.sonic@gmail.com>
2025-04-08 11:57:02 -0500

    Inicio.js

--- src/pages/Inicio.js
+++ src/pages/Inicio.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import Beneficios from '../components/Beneficios';
 const Inicio = () => {
   return (
     <div>
       <h1>Página de Inicio</h1>
       <p>Bienvenido a la página de inicio. Aquí encontrarás información general.</p>
+      <Beneficios/>
     </div>
   );
 };
